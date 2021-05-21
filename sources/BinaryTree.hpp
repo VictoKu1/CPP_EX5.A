148347c7d3638a94019cb6bc33d30c44a02da5ee
Victor <kushnir.victor@gmail.com>
2021-05-21 04:01:27 +0300

    .

---
+++ sources/BinaryTree.hpp
@@ -0,0 +1,28 @@
+#include "doctest.h"
+using namespace std;
+namespace ariel {
+template <typename T> class BinaryTree {
+private:
+  BinaryTree<T> *root;
+  BinaryTree<T> *left;
+  BinaryTree<T> *right;
+  T data;
+  int size;
+  int rightSize;
+  int leftSize;
+
+public:
+  BinaryTree() {}
+  ~BinaryTree() {}
+  BinaryTree<T> add_root(T *newRoot);
+  BinaryTree<T> add_left(T *newLeft);
+  BinaryTree<T> add_right(T *newRight);
+  //*TODO.
+  //*begin_preorder().
+  //*end_preorder().
+  //*begin_inorder().
+  //*end_inorder().
+  //*begin_postorder().
+  //*end_postorder().
+};
+} // namespace ariel
